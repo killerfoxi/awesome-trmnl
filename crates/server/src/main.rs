36cb35f358f04478a02d28e222c321da734360db
Rene Nussbaumer <killerfoxi@gmail.com>
2025-02-09 16:02:07 +0100

    Use maud

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -2,17 +2,22 @@ use std::sync::Arc;
 
 use axum::{
     body::Body,
-    extract::State,
+    extract::{Path, State},
     http::{Response, StatusCode},
-    response::{Html, IntoResponse},
+    response::IntoResponse,
     routing::get,
     Router,
 };
 use log::{error, info};
+use maud::{html, Markup};
 use tokio::net::TcpListener;
+use url::Url;
+
+mod pages;
 
 #[derive(Debug)]
 enum Error {
+    InvalidId,
     RenderError,
 }
 
@@ -30,11 +35,11 @@ impl IntoResponse for Error {
 
 async fn render_screen(
     renderer: &blender::Instance,
-    url: &str,
+    url: Url,
 ) -> axum::response::Result<Box<[u8]>> {
     info!("Requested rendering of: {url}");
     let img = renderer
-        .render(url)
+        .render(url.as_str())
         .await
         .inspect_err(|e| error!("Rendering error: {e:?}"))
         .map(|i| i.into_grayscaled())
@@ -44,34 +49,62 @@ async fn render_screen(
     Ok(writer.into_inner().into_boxed_slice())
 }
 
-#[axum::debug_handler]
-async fn render_test_screen(
+async fn render_screen_img(
     State(state): State<Arc<ServerState>>,
+    Path(id): Path<String>,
 ) -> axum::response::Result<Box<[u8]>> {
-    render_screen(&state.renderer, "http://localhost:8223/screen/test.html").await
+    render_screen(
+        &state.renderer,
+        state.render_url.join(&id).map_err(|_| Error::InvalidId)?,
+    )
+    .await
+}
+
+async fn screen_content(Path(id): Path<String>) -> Markup {
+    pages::test_screen()
+}
+
+async fn preview(Path(id): Path<String>) -> Markup {
+    pages::index(html! {
+        h1 { "Preview TRMNL screen" }
+        img src={"/screen/" (id)};
+    })
 }
 
 struct ServerState {
     renderer: blender::Instance,
+    render_url: Url,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
     colog::init();
+
+    let mut url = Url::parse("http://localhost/content/")?;
+    url.set_port(Some(8223)).unwrap();
     let state = Arc::new(ServerState {
         renderer: blender::Instance::new().await.unwrap(),
+        render_url: url,
     });
     let app = Router::new()
         .route(
             "/",
-            get(|| async { Html(r#"<html><body><img src="/screen/test"><body></html>"#) }),
+            get(pages::index(html! {
+                h1 { "Welcome to Awesome TRMNL." }
+                p { "Do you have a TRMNL device? Point it at me." }
+                p { "Or see a " a href="/preview/test" { "test preview" } "."}
+            })),
         )
-        .route("/screen/test", get(render_test_screen))
-        .route(
-            "/screen/test.html",
-            get(|| async { Html(include_str!("../assets/test.screen.html")) }),
-        )
+        .route("/content/{id}", get(screen_content))
+        .route("/screen/{id}", get(render_screen_img))
+        .route("/preview/{id}", get(preview))
         .with_state(state);
-    let listener = TcpListener::bind("0.0.0.0:8223").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8223").await?;
+    info!(
+        "Successfully started listening on {}",
+        listener.local_addr()?
+    );
+    axum::serve(listener, app).await?;
+    Ok(())
 }
---
+++ crates/server/src/pages.rs
@@ -0,0 +1,51 @@
+use maud::{html, Markup, DOCTYPE};
+
+pub fn index(inner: Markup) -> Markup {
+    html! {
+        (DOCTYPE)
+        title { "Awesome TRMNL" }
+        body {
+            (inner)
+        }
+    }
+}
+
+pub fn test_screen() -> Markup {
+    screen(html! {
+        div ."view view--full" {
+            div .layout {
+                div .columns {
+                    div .column {
+                        div .markdown {
+                            span .title { "Motivational Quote" }
+                            div ."content content--center" {
+                                r#"“I love inside jokes. I hope to be a part of one
+                                    someday.”"#
+                            }
+                            span ."label label--underline" { "Michael Scott" }
+                        }
+                    }
+                }
+            }
+
+            div .title_bar {
+                img .image src="https://usetrmnl.com/images/plugins/trmnl--render.svg";
+                span .title { "Plugin Title" }
+                span .instance { "Instance Title" }
+            }
+        }
+    })
+}
+
+pub fn screen(inner: Markup) -> Markup {
+    html! {
+        (DOCTYPE)
+        title { "Awesome TRMNL" }
+        link rel="stylesheet" href="https://usetrmnl.com/css/latest/plugins.css";
+        body .environment.trmnl {
+            div .screen {
+                (inner)
+            }
+        }
+    }
+}
